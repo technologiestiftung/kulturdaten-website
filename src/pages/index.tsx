47ee448fc2d493a35509080743d80d19ac9eb7b9
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-04 16:12:11 +0200

    Add Storybook and example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello world</main>
+      <main>
+        Hello world
+        <br />
+        <Link href="https://www.technologiestiftung-berlin.de/">Go somewhere else</Link>
+      </main>
     </>
   );
 }
