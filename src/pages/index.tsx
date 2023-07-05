ae4c1344796ed0367cf6243f3104faaaaaee666c
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-05 16:35:39 +0200

    Update favicon (and HTML tite) (#5)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TypeScript Template</title>
+        <title>Frontend Template</title>
         <meta name="description" content="Some description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
