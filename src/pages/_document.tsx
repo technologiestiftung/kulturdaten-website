837a0d7f3181be67de3d07daf3b6f9567da9598c
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-06 11:29:02 +0200

    Add global styles via Emotion

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import GlobalStyles from "../components/GlobalStyles";
 
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
+			<GlobalStyles />
 			<body>
 				<Main />
 				<NextScript />
