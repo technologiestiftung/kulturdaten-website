75ffa5851e59214ef39e2defacbaab3462d28ee7
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-13 15:48:07 +0200

    Implement graceful shutdown according to Next.js docs
    https://nextjs.org/docs/app/building-your-application/deploying#manual-graceful-shutdowns

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,17 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import GlobalStyles from "../components/GlobalStyles";
 
+if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+	process.on("SIGTERM", () => {
+		console.log("Received SIGTERM: ", "cleaning up");
+		process.exit(0);
+	});
+	process.on("SIGINT", () => {
+		console.log("Received SIGINT: ", "cleaning up");
+		process.exit(0);
+	});
+}
+
 export default function Document() {
 	return (
 		<Html lang="en">
