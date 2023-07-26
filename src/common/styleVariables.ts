867bd57620c3a5e3e2849d9cd288eafed55d8007
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-26 13:41:53 +0200

    Add some more global styles and our font-families

---
+++ src/common/styleVariables.ts
@@ -0,0 +1,25 @@
+export const fontFamilies = {
+	default: "ClanPro, sans-serif",
+};
+
+export const fontSizes = {
+	default: "18px",
+};
+
+export const lineHeights = {
+	default: 1.6,
+};
+
+export const colors = {
+	black: "#3b3b3a",
+	blueLight: "#a8daf4",
+	blueDark: "#1e3791",
+	red40: "#f5afa5",
+	lind60: "#c3dcb9",
+	yellow50: "#f0f0b4",
+	grayLight: "#f9f9f9",
+};
+
+export const spacings = {
+	get: (factor = 1.0) => factor * 5,
+};
