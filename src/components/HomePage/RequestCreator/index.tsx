3e9e1bf82eabb97e0ca53c1e82f694b8665d0e80
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-07-27 15:47:49 +0200

    Externalize request logic into useRequest hook

--- src/components/HomePage/RequestCreator/index.tsx
+++ src/components/HomePage/RequestCreator/index.tsx
@@ -8,17 +8,22 @@ import Block from "./Block";
 import ButtonContainer from "./ButtonContainer";
 import { REQUESTS, Request } from "./requests";
 
-export default function RequestCreator() {
+function useRequest() {
 	const [request, setRequest] = useState<Request | null>(null);
-	const t = useTranslations("Home");
-	// @ts-ignore No TypeScript support for dynamically-created i18n keys.
-	const requestText = request ? t(`for-interested.${request.i18nKey}`) : "";
-	const randomizeRequest = useCallback(async () => {
+	const randomizeRequest = useCallback(() => {
 		const currentIndex = request ? REQUESTS.indexOf(request) : -1;
 		const nextIndex = getRandomIndexWithout(REQUESTS, currentIndex);
 		const nextRequest = REQUESTS[nextIndex];
 		setRequest(nextRequest);
 	}, [request]);
+	return { request, randomizeRequest };
+}
+
+export default function RequestCreator() {
+	const { request, randomizeRequest } = useRequest();
+	const t = useTranslations("Home");
+	// @ts-ignore No TypeScript support for dynamically-created i18n keys.
+	const requestText = request ? t(`for-interested.${request.i18nKey}`) : "";
 	return (
 		<Block>
 			<ButtonContainer>
