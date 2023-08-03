72b84591f81813706c04523a91ca4fc0384a2725
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-03 14:45:40 +0200

    Move global styles to _app (alongside the global tooltip styles)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from "next-intl";
 import type { AppProps } from "next/app";
 import "react-tooltip/dist/react-tooltip.css";
+import GlobalStyles from "../components/GlobalStyles";
 
 type CustomPageProps = {
 	messages: IntlMessages;
@@ -9,6 +10,7 @@ type CustomPageProps = {
 export default function App({ Component, pageProps }: AppProps<CustomPageProps>) {
 	return (
 		<NextIntlClientProvider messages={pageProps.messages}>
+			<GlobalStyles />
 			<Component {...pageProps} />
 		</NextIntlClientProvider>
 	);
