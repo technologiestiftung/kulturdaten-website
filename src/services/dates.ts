8889133e1f27ed7c30b154df1364ce27468b4b55
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-03 17:15:37 +0200

    Do not hard-code Berlin timezone in date service

--- src/services/dates.ts
+++ src/services/dates.ts
@@ -2,9 +2,6 @@ import { Locale } from "../hooks/useLocale";
 
 export default function formatDate(date: Date | string, locale: Locale, options?: Intl.DateTimeFormatOptions) {
 	const dateObject = typeof date === "string" ? new Date(date) : date;
-	const format = new Intl.DateTimeFormat(locale, {
-		timeZone: "Europe/Berlin",
-		...options,
-	});
+	const format = new Intl.DateTimeFormat(locale, options);
 	return format.format(dateObject);
 }
