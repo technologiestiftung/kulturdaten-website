eba58b4018902f95ee8ec1e534faff4819543397
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-03 17:00:18 +0200

    Use correct color for TextCursor

--- src/components/HomePage/RequestCreatorAndList/RequestCreator/AnimatedText/TextCursor.tsx
+++ src/components/HomePage/RequestCreatorAndList/RequestCreator/AnimatedText/TextCursor.tsx
@@ -1,6 +1,5 @@
-import styled from "@emotion/styled";
-import { colors } from "../../../../../common/styleVariables";
 import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
 
 const blink = keyframes({
 	"0%": { opacity: 1 },
@@ -13,8 +12,7 @@ const Container = styled.div({
 	fontSize: "1.1em",
 	lineHeight: 1.0,
 	display: "inline-flex",
-	color: colors.black,
-	animation: `${blink} 1s infinite`,
+	animation: `${blink} 1s linear infinite`,
 	userSelect: "none",
 });
 
