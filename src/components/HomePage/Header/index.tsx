e3ccc2c91f381d5eb90957e154d8d947dcdeb75f
Urs Schmidt <urs.schmidt@ts.berlin>
2023-08-08 16:43:19 +0200

    Add Header Component

---
+++ src/components/HomePage/Header/index.tsx
@@ -0,0 +1,122 @@
+import styled from "@emotion/styled";
+import {
+	borderRadiuses,
+	breakpoints,
+	colors,
+	fontSizes,
+	fontWeights,
+	mediaQueries,
+	timings,
+} from "../../../common/styleVariables";
+import { AnchorLinks } from "..";
+import { useTranslations } from "next-intl";
+
+const HeaderContainer = styled.div({
+	position: "fixed",
+	left: 0,
+	width: "100vw",
+	zIndex: 100,
+	color: colors.blueDark,
+	borderBottom: `3px solid ${colors.blueDark}`,
+	backgroundColor: colors.white,
+});
+
+const ContentWrapper = styled.div({
+	maxWidth: breakpoints.l,
+	margin: "0 auto",
+	display: "flex",
+	gap: "0.8rem",
+	alignItems: "center",
+	justifyContent: "space-between",
+	padding: "0 1.4rem",
+	height: "100%",
+	[mediaQueries.s]: {
+		flexWrap: "wrap",
+		alignItems: "center",
+	},
+});
+
+const LogoWrapper = styled.div({
+	display: "flex",
+	alignItems: "center",
+	gap: ".8rem",
+});
+
+const Logo = styled.div({
+	width: "2rem",
+	height: "2rem",
+	backgroundColor: colors.blueDark,
+	borderRadius: borderRadiuses.round,
+});
+
+const Title = styled.span({
+	fontWeight: fontWeights.medium,
+	fontSize: fontSizes.small,
+});
+
+const LightTitle = styled.span({
+	fontWeight: fontWeights.light,
+});
+
+const AnchorLinkWrapper = styled.div({
+	display: "flex",
+	flexDirection: "column",
+	flexWrap: "wrap",
+	justifyContent: "space-between",
+	alignItems: "flex-end",
+	width: "100%",
+	maxWidth: "500px",
+	gap: "1.6rem",
+	[mediaQueries.s]: {
+		flexDirection: "row",
+	},
+});
+
+const AnchorLink = styled.a<{ active: boolean }>((props) => ({
+	color: colors.blueDark,
+	textDecoration: props.active ? "underline" : "none",
+	fontWeight: props.active ? fontWeights.medium : fontWeights.light,
+	fontSize: fontSizes.small,
+	transition: `all ${timings.short} ease-in-out`,
+	"&:hover": {
+		textDecoration: "underline",
+		fontWeight: fontWeights.medium,
+	},
+}));
+
+interface Props {
+	activeAnchorLink: AnchorLinks | null;
+}
+
+const Header = ({ activeAnchorLink }: Props) => {
+	const t = useTranslations("Home.header");
+	return (
+		<HeaderContainer>
+			<ContentWrapper>
+				<LogoWrapper>
+					<Logo />
+					<Title>
+						KULTURDATEN
+						<LightTitle>BERLIN</LightTitle>
+					</Title>
+				</LogoWrapper>
+				<AnchorLinkWrapper>
+					<AnchorLink
+						active={activeAnchorLink === AnchorLinks.INTERESTEDSECTION}
+						href={`#${AnchorLinks.INTERESTEDSECTION}`}
+					>
+						{t("link-interested")}
+					</AnchorLink>
+					<AnchorLink active={activeAnchorLink === AnchorLinks.ARTISTSECTION} href={`#${AnchorLinks.ARTISTSECTION}`}>
+						{t("link-artists")}
+					</AnchorLink>
+					<AnchorLink active={activeAnchorLink === AnchorLinks.DATASECTION} href={`#${AnchorLinks.DATASECTION}`}>
+						{t("link-data")}
+					</AnchorLink>
+				</AnchorLinkWrapper>
+			</ContentWrapper>
+		</HeaderContainer>
+	);
+};
+
+export default Header;
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -7,23 +7,73 @@ import IntroSection from "./IntroSection";
 import NewsletterSection from "./NewsletterSection";
 import RequestCreatorAndList from "./RequestCreatorAndList";
 import Section from "./Section";
+import Header from "./Header";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
 
 const Main = styled.main(() => ({
 	width: `min(100%, ${widths.maxContentWidth})`,
 	margin: "0 auto",
 }));
 
+export interface InView {
+	interestedSectionInView: boolean;
+	artistSectionInView: boolean;
+	dataSectionInView: boolean;
+}
+
+export enum AnchorLinks {
+	INTERESTEDSECTION = "interestedSection",
+	ARTISTSECTION = "artistSection",
+	DATASECTION = "dataSection",
+}
+
+const inViewOptions = {
+	threshold: 0.5,
+};
 export default function HomePage() {
+	const [activeAnchorLink, setActiveAnchorLink] = useState<AnchorLinks | null>(null);
+	const getLatestInView = (inView: InView) => {
+		const inViewKeys = Object.keys(inView) as Array<keyof InView>;
+		const trueInViewKeys = inViewKeys.filter((key) => inView[key]);
+
+		return trueInViewKeys.length === 0
+			? null
+			: trueInViewKeys.length === 1
+			? trueInViewKeys[0]
+			: trueInViewKeys[trueInViewKeys.length - 1];
+	};
+
+	const { ref: interestedSectionRef, inView: interestedSectionInView } = useInView(inViewOptions);
+	const { ref: artistSectionRef, inView: artistSectionInView } = useInView(inViewOptions);
+	const { ref: dataSectionRef, inView: dataSectionInView } = useInView(inViewOptions);
+
+	useEffect(() => {
+		const latestInView = getLatestInView({ interestedSectionInView, artistSectionInView, dataSectionInView });
+		setActiveAnchorLink(
+			latestInView === "interestedSectionInView"
+				? AnchorLinks.INTERESTEDSECTION
+				: latestInView === "artistSectionInView"
+				? AnchorLinks.ARTISTSECTION
+				: latestInView === "dataSectionInView"
+				? AnchorLinks.DATASECTION
+				: null
+		);
+	}, [artistSectionInView, interestedSectionInView, dataSectionInView]);
+
 	return (
 		<>
 			<Head />
 			<Main>
+				<Header activeAnchorLink={activeAnchorLink} />
 				<Section>
 					<IntroSection />
-					<RequestCreatorAndList />
 				</Section>
 				<NewsletterSection />
-				<Section>
+				<Section ref={interestedSectionRef} id={AnchorLinks.INTERESTEDSECTION}>
+					<RequestCreatorAndList />
+				</Section>
+				<Section ref={artistSectionRef} id={AnchorLinks.ARTISTSECTION}>
 					<ArtistSection />
 				</Section>
 				<NewsletterSection />
