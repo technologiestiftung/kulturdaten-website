cef17474adc15a7d4f13b31c8c8134932f615a81
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-08 17:04:47 +0200

    Make Spacer work properly in flex contexts

--- src/components/Spacer/index.tsx
+++ src/components/Spacer/index.tsx
@@ -7,5 +7,14 @@ type Props = {
 };
 
 export default function Spacer({ size }: Props) {
-	return <StyledDiv role="none" style={{ width: size, height: size }} />;
+	return (
+		<StyledDiv
+			role="none"
+			style={{
+				width: size,
+				height: size,
+				flex: `0 0 ${size}px`,
+			}}
+		/>
+	);
 }
