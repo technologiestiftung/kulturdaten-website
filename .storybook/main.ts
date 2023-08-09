980d088f81e1008a6d00caa4c448eab05c871501
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-09 11:30:46 +0200

    Properly load our global styles in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+	staticDirs: ["../public"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/addon-styling",
+	],
 	framework: {
 		name: "@storybook/nextjs",
 		options: {},
