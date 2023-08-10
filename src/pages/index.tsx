9fd1621011da1e9b04be11d55bb9121fad734a17
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-10 14:05:54 +0200

    Simplify HomePage’s i18n message fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import type { GetServerSideProps } from "next";
+import type { GetStaticProps } from "next";
 import HomePage from "../components/HomePage";
 import { loadMessages } from "../services/i18n";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	return {
-		props: {
-			messages: await loadMessages(context.locale!),
-		},
-	};
-};
+export const getStaticProps: GetStaticProps = async (context) => ({
+	props: {
+		messages: await loadMessages(context.locale!),
+	},
+});
 
 export default function Home() {
 	return <HomePage />;
