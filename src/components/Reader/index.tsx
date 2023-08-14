be509007817c676fa92da5c27aa6f09e4f0c02be
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-14 17:05:32 +0200

    Add privacy policy page content

--- src/components/Reader/index.tsx
+++ src/components/Reader/index.tsx
@@ -9,7 +9,7 @@ const Reader = styled.div({
 	backgroundColor: colors.white,
 	padding: `0 ${spacings.horizontalPagePadding}`,
 	...textStyles,
-	h2: {
+	"h2, h3, h4": {
 		margin: `${spacings.get(4)}px 0 ${spacings.get(1)}px 0`,
 	},
 	p: {
@@ -17,6 +17,9 @@ const Reader = styled.div({
 		"&:last-of-type": {
 			margin: 0,
 		},
+	},
+	ul: {
+		margin: `0 0 ${spacings.get(2)}px 0`,
 	},
 });
 
