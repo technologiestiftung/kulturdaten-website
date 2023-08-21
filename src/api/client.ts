6636068cebca3809901aee52b5710d192c9b8ea7
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-21 15:30:45 +0200

    Fix API base URL configuration

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ import { OpenAPI } from "./client/core/OpenAPI";
  * Use this client for all API requests.
  */
 const apiClient = new APIClient({
-	BASE: process.env.API_BASE_URL || OpenAPI.BASE,
+	BASE: process.env.NEXT_PUBLIC_API_BASE_URL || OpenAPI.BASE,
 });
 
 export default apiClient;
