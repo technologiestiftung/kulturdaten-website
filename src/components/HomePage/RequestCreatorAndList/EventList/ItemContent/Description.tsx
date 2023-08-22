ab31ae169517d4d1646a6f258279218b3e5b9e36
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-22 10:24:56 +0200

    Add description expand feature (#39)

---
+++ src/components/HomePage/RequestCreatorAndList/EventList/ItemContent/Description.tsx
@@ -0,0 +1,119 @@
+import styled from "@emotion/styled";
+import { useTranslations } from "next-intl";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { borderRadiuses, colors, fontSizes, spacings, timings } from "../../../../../common/styleVariables";
+import Button from "../../../../Button";
+import Icon from "../../../../Icon";
+
+const readMoreButtonClass = "read-more-button";
+
+const Container = styled.div(({ onClick }) => ({
+	position: "relative",
+	color: colors.black,
+	overflow: "hidden",
+	cursor: onClick ? "pointer" : undefined,
+	[`& .${readMoreButtonClass}`]: {
+		opacity: 0,
+	},
+	"@media (hover: hover)": {
+		"&:hover, &:focus-within": {
+			[`.${readMoreButtonClass}`]: {
+				opacity: 1,
+			},
+		},
+	},
+}));
+
+const Gradient = styled.div({
+	position: "absolute",
+	bottom: 0,
+	left: 0,
+	width: "100%",
+	height: "70%",
+	background: `linear-gradient(0deg, ${colors.turquoise} 0%, rgba(160, 250, 242, 0) 100%)`,
+	transition: `opacity ${timings.medium} ease-in-out`,
+	pointerEvents: "none",
+});
+
+const ReadMoreContainer = styled.div({
+	position: "absolute",
+	bottom: 0,
+	left: 0,
+	width: "100%",
+	textAlign: "center",
+});
+
+const ReadMoreButton = styled(Button)({
+	backgroundColor: colors.white,
+	color: colors.blueDark,
+	cursor: "pointer",
+	fontSize: fontSizes.small,
+	display: "inline-flex",
+	alignItems: "center",
+	gap: spacings.get(0.5),
+	padding: `${spacings.get(1)}px ${spacings.get(2)}px`,
+	borderRadius: borderRadiuses.medium,
+	transition: `opacity ${timings.short} ease-in-out`,
+});
+
+function sanitizeDescription(description: string) {
+	return description.replace(/&shy;/g, "").replace(/(\n)$/gm, "");
+}
+
+type Props = {
+	description: string;
+	attractionId: string;
+	onExpanded(): void;
+};
+
+export default function Description({ description, attractionId, onExpanded }: Props) {
+	const t = useTranslations("Home.for-interested");
+	const textId = `description-${attractionId}`;
+	const textRef = useRef<HTMLParagraphElement>(null);
+	// We want to show ~3 lines of text in the collapsed state.
+	const collapsedHeight = 80;
+	const [expanded, setExpanded] = useState(false);
+	const [isInteractive, setIsInteractive] = useState(false);
+
+	const initialize = useCallback(() => {
+		const element = textRef.current!;
+		const { height } = element.getBoundingClientRect();
+		const isExpandable = height > collapsedHeight;
+		if (isExpandable) {
+			setIsInteractive(true);
+		} else {
+			setExpanded(true);
+		}
+	}, []);
+
+	useEffect(() => initialize(), [initialize]);
+
+	const expand = useCallback(() => setExpanded(true), []);
+	useEffect(() => {
+		if (isInteractive && expanded) {
+			onExpanded();
+		}
+	}, [expanded, isInteractive, onExpanded]);
+
+	const a11yButtonProps = isInteractive ? { "aria-expanded": expanded, "aria-controls": textId } : {};
+
+	return (
+		<Container
+			style={{ height: expanded ? undefined : collapsedHeight }}
+			onClick={!expanded ? () => expand() : undefined}
+			{...a11yButtonProps}
+		>
+			<p ref={textRef} id={textId}>
+				{sanitizeDescription(description)}
+			</p>
+			{isInteractive && <Gradient role="none" style={{ opacity: expanded ? 0 : 1 }} />}
+			{!expanded && (
+				<ReadMoreContainer>
+					<ReadMoreButton unstyled={true} onClick={expand} className={readMoreButtonClass} {...a11yButtonProps}>
+						<Icon name="chevron-down" size={18} /> {t("description-read-more")}
+					</ReadMoreButton>
+				</ReadMoreContainer>
+			)}
+		</Container>
+	);
+}
--- src/components/HomePage/RequestCreatorAndList/EventList/ItemContent/index.tsx
+++ src/components/HomePage/RequestCreatorAndList/EventList/ItemContent/index.tsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
 import { useCallback } from "react";
-import { Event } from "../../../../api/client/models/Event";
-import { colors, fontSizes, fontWeights, lineHeights } from "../../../../common/styleVariables";
-import { Locale, fallbackLocale } from "../../../../hooks/useLocale";
-import { EventWithAttraction } from "../../../../services/apiRequests";
-import formatDate from "../../../../services/dates";
-import Spacer from "../../../Spacer";
+import { Event } from "../../../../../api/client/models/Event";
+import { fontSizes, fontWeights, lineHeights } from "../../../../../common/styleVariables";
+import { Locale, fallbackLocale } from "../../../../../hooks/useLocale";
+import { EventWithAttraction } from "../../../../../services/apiRequests";
+import formatDate from "../../../../../services/dates";
+import Spacer from "../../../../Spacer";
+import Description from "./Description";
 
 const Meta = styled.div({
 	fontSize: fontSizes.small,
@@ -18,37 +19,17 @@ const Title = styled.div({
 	fontWeight: fontWeights.medium,
 });
 
-const Description = styled.div({
-	position: "relative",
-	maxHeight: "5rem",
-	overflow: "hidden",
-	color: colors.black,
-});
-
-const DescriptionFade = styled.div({
-	position: "absolute",
-	bottom: 0,
-	left: 0,
-	width: "100%",
-	height: "70%",
-	background: `linear-gradient(0deg, ${colors.turquoise} 0%, rgba(160, 250, 242, 0) 100%)`,
-	pointerEvents: "none",
-});
-
 type Props = {
 	eventWithAttraction: EventWithAttraction;
 	locale: Locale;
+	onExpandedDescription(): void;
 };
 
 function getStartDateAsISO(event: Event) {
 	return `${event.schedule?.startDate}T${event.schedule?.startTime}`;
 }
 
-function sanitizeDescription(description: string) {
-	return description.replace(/(\n)*$/gm, "");
-}
-
-export default function ItemContent({ eventWithAttraction, locale }: Props) {
+export default function ItemContent({ eventWithAttraction, locale, onExpandedDescription }: Props) {
 	const { event, attraction } = eventWithAttraction;
 	const isoDate = getStartDateAsISO(event);
 	const getLocalizedContent = useCallback(
@@ -69,10 +50,11 @@ export default function ItemContent({ eventWithAttraction, locale }: Props) {
 			<Spacer size={10} />
 			<Title>{getLocalizedContent(attraction?.title)}</Title>
 			<Spacer size={10} />
-			<Description>
-				<p>{sanitizeDescription(getLocalizedContent(attraction?.description))}</p>
-				<DescriptionFade role="none" />
-			</Description>
+			<Description
+				description={getLocalizedContent(attraction?.description)}
+				attractionId={attraction?.identifier || ""}
+				onExpanded={onExpandedDescription}
+			/>
 		</>
 	);
 }
