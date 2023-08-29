65e4fdd9e757363987752e3570b55604dbf990c1
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-29 10:11:51 +0200

    fix: remove culture interested section

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -6,7 +6,6 @@ import ArtistSection from "../ArtistSection";
 import Header from "../Header";
 import Page from "../Page";
 import Spacer from "../Spacer";
-import CultureInterestedSection from "./CultureInterestedSection";
 import DataUsersSection from "./DataUsersSection";
 import IntroSection from "./IntroSection";
 import NewsletterSection from "./NewsletterSection";
@@ -14,7 +13,6 @@ import RequestCreatorAndList from "./RequestCreatorAndList";
 import Section from "./Section";
 
 interface InView {
-	interestedSectionInView: boolean;
 	artistSectionInView: boolean;
 	dataSectionInView: boolean;
 }
@@ -27,31 +25,23 @@ export default function HomePage() {
 	const t = useTranslations("Home");
 	const [activeAnchorLink, setActiveAnchorLink] = useState<AnchorLinks | null>(null);
 	const getLatestInView = (inView: InView) => {
-		const orderedSections: Array<keyof InView> = [
-			"interestedSectionInView",
-			"artistSectionInView",
-			"dataSectionInView",
-		];
+		const orderedSections: Array<keyof InView> = ["artistSectionInView", "dataSectionInView"];
 		const trueInViewKeys = orderedSections.filter((key) => inView[key]);
-
 		return trueInViewKeys[trueInViewKeys.length - 1] || null;
 	};
 
-	const { ref: interestedSectionRef, inView: interestedSectionInView } = useInView(inViewOptions);
 	const { ref: artistSectionRef, inView: artistSectionInView } = useInView(inViewOptions);
 	const { ref: dataSectionRef, inView: dataSectionInView } = useInView(inViewOptions);
 
 	useEffect(() => {
-		const latestInView = getLatestInView({ interestedSectionInView, artistSectionInView, dataSectionInView });
-
+		const latestInView = getLatestInView({ artistSectionInView, dataSectionInView });
 		const linkMap: Record<keyof InView, AnchorLinks> = {
-			interestedSectionInView: AnchorLinks.INTERESTEDSECTION,
 			artistSectionInView: AnchorLinks.ARTISTSECTION,
 			dataSectionInView: AnchorLinks.DATASECTION,
 		};
 		const newAnchorLink = latestInView ? linkMap[latestInView] : null;
 		setActiveAnchorLink(newAnchorLink);
-	}, [artistSectionInView, interestedSectionInView, dataSectionInView]);
+	}, [artistSectionInView, dataSectionInView]);
 
 	return (
 		<Page
@@ -69,9 +59,6 @@ export default function HomePage() {
 			</Section>
 			<Section ref={artistSectionRef} id={AnchorLinks.ARTISTSECTION}>
 				<ArtistSection />
-			</Section>
-			<Section ref={interestedSectionRef} id={AnchorLinks.INTERESTEDSECTION}>
-				<CultureInterestedSection />
 			</Section>
 			<Section ref={dataSectionRef} id={AnchorLinks.DATASECTION}>
 				<DataUsersSection />
