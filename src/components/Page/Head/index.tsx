103aa62d416e7cb887602333f1b6dd43f7f59c09
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-08-29 10:12:10 +0200

    fix: fix path of svg favicon

--- src/components/Page/Head/index.tsx
+++ src/components/Page/Head/index.tsx
@@ -14,7 +14,7 @@ export default function Head({ metadata }: Props) {
 			<meta name="description" content={description} />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="icon" href="/favicon.ico" />
-			<link rel="icon" type="image/svg+xml" href="/icon.svg" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
